0465546f4ec12396c43589635b0bb67452371e60
Salvio Quiralte <8882468+salvioq@users.noreply.github.com>
2022-02-14 20:07:58 +0100

    Actualiza 20220214

--- Northwind_Cust_EF_Xaml/Northwind_Cust_EF_Xaml/Northwind_Model.Designer.cs
+++ Northwind_Cust_EF_Xaml/Northwind_Cust_EF_Xaml/Northwind_Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'U:\src\salvioq_gitrepo\DamSoluciones\AgendaContactos_DAM\AgendaContactos_DAM\ContactosModelo.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'U:\src\salvioq_gitrepo\DamSoluciones\Northwind_Cust_EF_Xaml\Northwind_Cust_EF_Xaml\Northwind_Model.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
